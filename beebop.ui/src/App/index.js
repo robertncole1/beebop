5901679db5a3e2fb7b82f86d9258664a7a2b841a
Rob Cole <robertncole1@gmail.com>
2021-12-13 22:14:58 -0500

    added in a lot of front-end stuff, user auth, and some styling

--- beebop.ui/src/App/index.js
+++ beebop.ui/src/App/index.js
@@ -1,36 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'firebase/auth';
 import './App.scss';
+import Routes from '../helpers/Routes';
+import NavBar from '../components/NavBar';
+import { getSingleUserByGoogleId } from '../helpers/data/userData';
+// import Footer from '../components/Footer';
 
 function App() {
-  const [domWriting, setDomWriting] = useState('Nothing Here!');
-
-  const handleClick = (e) => {
-    console.warn(`You clicked ${e.target.id}`);
-    setDomWriting(`You clicked ${e.target.id}! Check the Console!`);
-  };
-
+  // When you set up firebase add setUser method and change useState to null.
+  const [user, setUser] = useState(null);
+  // Checking for authenticated users. You must set up firebase authentication for this to work!
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        // eslint-disable-next-line no-undef
+        authed.getIdToken().then((token) => localStorage.setItem('token', token));
+        getSingleUserByGoogleId(authed.uid).then((response) => setUser(response));
+      } else {
+        setUser(false);
+      }
+    });
+  }, []);
   return (
     <div className='App'>
-      <h2>INSIDE APP COMPONENT</h2>
-      <div>
-        <button
-          id='this-button'
-          className='btn btn-info'
-          onClick={handleClick}
-        >
-          I am THIS button
-        </button>
-      </div>
-      <div>
-        <button
-          id='that-button'
-          className='btn btn-primary mt-3'
-          onClick={handleClick}
-        >
-          I am THAT button
-        </button>
-      </div>
-      <h3>{domWriting}</h3>
+      <Router>
+        <NavBar user={user} setUser={setUser}/>
+        <Routes user={user} setUser={setUser}/>
+        {/* <Footer/> */}
+      </Router>
     </div>
   );
 }
