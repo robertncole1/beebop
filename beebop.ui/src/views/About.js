32a462bce414cb54bb9e6f045168f1dbdad89394
Rob Cole <robertncole1@gmail.com>
2021-12-17 16:57:12 -0500

    edited about us view

--- beebop.ui/src/views/About.js
+++ beebop.ui/src/views/About.js
@@ -4,19 +4,24 @@ import { Col, Container, Row } from 'reactstrap';
 
 function About() {
   return (
-    <>
+    <div className='about-cont'>
     <Container>
       <Row>
         <Col>
-          <h2>About Us</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <img
+          src="https://user-images.githubusercontent.com/76854545/146611653-325212df-e46a-4ad8-83bd-fe911edd7367.png"
+          width="600"
+          alt="beebop about us"
+        />
         </Col>
         <Col>
-          <h2>FORM</h2>
+          <h2>About Us</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </Col>
       </Row>
     </Container>
-  </>
+  </div>
   );
 }
 
